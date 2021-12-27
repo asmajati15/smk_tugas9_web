f1beb3260c30051891212a960927324de2987fbf
asmajati15 <asmajati.cooler15@gmail.com>
2021-12-27 22:22:56 +0700

    tugas ci4

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,174 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="/css/style.css">
+
+	<title>Salakanagara Bus</title>
+</head>
+
+<body>
+
+	<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+		<div class="container-fluid">
+			<a class="navbar-brand" id="namapo" href="#"><i>SALAKANAGARA</i></a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link" aria-current="page" href="#">Beranda</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Kelas Armada</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Penyewaan</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Tentang Kami</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+		<div class="carousel-indicators">
+			<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+			<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+		</div>
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img src="/img/carousel2.jpg" class="d-block w-100" alt="Masterpiece Class Bus">
+				<div class="carousel-caption d-none d-md-block">
+					<h5>Masterpiece Class</h5>
+					<p>30 Seats + Toilet + Legrest</p>
+				</div>
+			</div>
+			<div class="carousel-item">
+				<img src="/img/carousel3.jpg" class="d-block w-100" alt="Bussiness Class Bus">
+				<div class="carousel-caption d-none d-md-block">
+					<h5>Bussiness Class</h5>
+					<p>50/59 Seats Non Toilet</p>
+				</div>
+			</div>
+		</div>
+		<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Previous</span>
+		</button>
+		<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Next</span>
+		</button>
+	</div>
+
+	<section id="slogan" class="slogan">
+		<div class="container mt-5 pb-5">
+			<div class="row">
+				<div class="col text-center">
+					<h2>Explore Indonesia with Us</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col text-center">
+					<p>Salakanagara memiliki komitmen agar para penumpang merasa puas, aman, dan nyaman saat menggunakan armada kami.
+						<br>Crew yang ramah, armada yang terawat, juga harga sewa yang bersahabat.
+					</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="reservation" class="reservation bg-light">
+		<div class="container">
+			<div class="row pt-5">
+				<div class="col text-center">
+					<h2>Armada Bus</h2>
+				</div>
+			</div>
+			<div class="row mt-5 pb-4">
+				<div class="col-md">
+					<div class="card">
+						<img src="/img/card1.jpg" class="card-img-top" alt="...">
+						<div class="card-body">
+							<h5 class="card-title">Masterpiece Class</h5>
+							<p class="card-text">Konfigurasi 2-2 + Legrest<br>Dispenser<br>Coolbox<br>Toilet<br>AVOD + LED TV 24"</p>
+							<a href="#" class="btn btn-danger">Booking Sekarang</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md mb-4">
+					<div class="card">
+						<img src="/img/card2.jpg" class="card-img-top" alt="...">
+						<div class="card-body">
+							<h5 class="card-title">Bussiness Class</h5>
+							<p class="card-text">Konfigurasi 2-2<br>Konfigurasi 2-3<br>Dispenser<br>Coolbox<br>LED TV 24"</p>
+							<a href="#" class="btn btn-danger">Booking Sekarang</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="address" class="address mb-5">
+		<div class="container">
+			<div class="row pt-5">
+				<div class="col">
+					<h2>Contact us</h2>
+				</div>
+			</div>
+			<div class="row mt-5">
+				<div class="col">
+					<h4>Telfon</h4>
+					<p>0838-7715-8482<br>0851-5784-8662</p>
+					<h4>Instagram</h4>
+					<p>@abisatya_group</p>
+				</div>
+				<div class="col">
+					<h4>Kantor Pusat</h4>
+					<p>Jl. Kol. Ahmad Syam, Katulampa, Kota Bogor</p>
+					<h4>Kantor Cabang</h4>
+					<p>Ngadiluwih, Kota Kediri</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<footer class="bg-dark text-white">
+		<div class="container">
+			<div class="row pt-2">
+				<div class="col text-center">
+					<p>Asmajati Ananto | 2021</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<div class="container">
+
+	</div>
+
+	<!-- Optional JavaScript; choose one of the two! -->
+
+	<!-- Option 1: Bootstrap Bundle with Popper -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+	<!-- Option 2: Separate Popper and Bootstrap JS -->
+	<!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
